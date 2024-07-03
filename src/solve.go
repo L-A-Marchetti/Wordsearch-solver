40dcd8258b7ca32758dcf3ad8138d3cfe8fb359c
lmarchet <lucasmarchetti21@gmail.com>
2024-07-03 18:15:28 +0200

    oblique directions implemented

--- src/solve.go
+++ src/solve.go
@@ -30,8 +30,9 @@ func Solve(wordCount int) {
 							count++
 						}
 						if word == WordSearchGrid.ListWords()[wordCount] {
-							WordSearchGrid.Grid[y][x] = "\033[" + Light + "" + Color + "m" + WordSearchGrid.Grid[y][x]
-							WordSearchGrid.Grid[y][x+count-1] += "\033[0m"
+							for i := 0; i < count; i++ {
+								WordSearchGrid.Grid[y][x+i] = "\033[" + Light + "" + Color + "m" + WordSearchGrid.Grid[y][x+i] + "\033[0m"
+							}
 							WordSearchGrid.DisplayGrid()
 							Save = append(Save, word)
 							wordFound = true
@@ -49,8 +50,9 @@ func Solve(wordCount int) {
 							count++
 						}
 						if word == WordSearchGrid.ListWords()[wordCount] {
-							WordSearchGrid.Grid[y][x-count+1] = "\033[" + Light + "" + Color + "m" + WordSearchGrid.Grid[y][x-count+1]
-							WordSearchGrid.Grid[y][x] += "\033[0m"
+							for i := 0; i < count; i++ {
+								WordSearchGrid.Grid[y][x-i] = "\033[" + Light + "" + Color + "m" + WordSearchGrid.Grid[y][x-i] + "\033[0m"
+							}
 							WordSearchGrid.DisplayGrid()
 							Save = append(Save, word)
 							wordFound = true
@@ -90,6 +92,86 @@ func Solve(wordCount int) {
 						if word == WordSearchGrid.ListWords()[wordCount] {
 							for i := 0; i < count; i++ {
 								WordSearchGrid.Grid[y-i][x] = "\033[" + Light + "" + Color + "m" + WordSearchGrid.Grid[y-i][x] + "\033[0m"
+							}
+							WordSearchGrid.DisplayGrid()
+							Save = append(Save, word)
+							wordFound = true
+							break
+						}
+						word = colums
+					}
+					// Search upper-right.
+					if y-1 >= 0 && x+1 < WordSearchGrid.Width && WordSearchGrid.Grid[y-1][x+1] == string(WordSearchGrid.ListWords()[wordCount][1]) {
+						word += WordSearchGrid.Grid[y-1][x+1]
+						count := 2
+						for y-count >= 0 && x+count < WordSearchGrid.Width && count < len(WordSearchGrid.ListWords()[wordCount]) &&
+							WordSearchGrid.Grid[y-count][x+count] == string(WordSearchGrid.ListWords()[wordCount][count]) {
+							word += WordSearchGrid.Grid[y-count][x+count]
+							count++
+						}
+						if word == WordSearchGrid.ListWords()[wordCount] {
+							for i := 0; i < count; i++ {
+								WordSearchGrid.Grid[y-i][x+i] = "\033[" + Light + "" + Color + "m" + WordSearchGrid.Grid[y-i][x+i] + "\033[0m"
+							}
+							WordSearchGrid.DisplayGrid()
+							Save = append(Save, word)
+							wordFound = true
+							break
+						}
+						word = colums
+					}
+					// Search upper-left.
+					if y-1 >= 0 && x-1 >= 0 && WordSearchGrid.Grid[y-1][x-1] == string(WordSearchGrid.ListWords()[wordCount][1]) {
+						word += WordSearchGrid.Grid[y-1][x-1]
+						count := 2
+						for y-count >= 0 && x-count >= 0 && count < len(WordSearchGrid.ListWords()[wordCount]) &&
+							WordSearchGrid.Grid[y-count][x-count] == string(WordSearchGrid.ListWords()[wordCount][count]) {
+							word += WordSearchGrid.Grid[y-count][x-count]
+							count++
+						}
+						if word == WordSearchGrid.ListWords()[wordCount] {
+							for i := 0; i < count; i++ {
+								WordSearchGrid.Grid[y-i][x-i] = "\033[" + Light + "" + Color + "m" + WordSearchGrid.Grid[y-i][x-i] + "\033[0m"
+							}
+							WordSearchGrid.DisplayGrid()
+							Save = append(Save, word)
+							wordFound = true
+							break
+						}
+						word = colums
+					}
+					// Search lower-right.
+					if y+1 < WordSearchGrid.Height && x+1 < WordSearchGrid.Width && WordSearchGrid.Grid[y+1][x+1] == string(WordSearchGrid.ListWords()[wordCount][1]) {
+						word += WordSearchGrid.Grid[y+1][x+1]
+						count := 2
+						for y+count < WordSearchGrid.Height && x+count < WordSearchGrid.Width && count < len(WordSearchGrid.ListWords()[wordCount]) &&
+							WordSearchGrid.Grid[y+count][x+count] == string(WordSearchGrid.ListWords()[wordCount][count]) {
+							word += WordSearchGrid.Grid[y+count][x+count]
+							count++
+						}
+						if word == WordSearchGrid.ListWords()[wordCount] {
+							for i := 0; i < count; i++ {
+								WordSearchGrid.Grid[y+i][x+i] = "\033[" + Light + "" + Color + "m" + WordSearchGrid.Grid[y+i][x+i] + "\033[0m"
+							}
+							WordSearchGrid.DisplayGrid()
+							Save = append(Save, word)
+							wordFound = true
+							break
+						}
+						word = colums
+					}
+					// Search lower-left.
+					if y+1 < WordSearchGrid.Height && x-1 >= 0 && WordSearchGrid.Grid[y+1][x-1] == string(WordSearchGrid.ListWords()[wordCount][1]) {
+						word += WordSearchGrid.Grid[y+1][x-1]
+						count := 2
+						for y+count < WordSearchGrid.Height && x-count >= 0 && count < len(WordSearchGrid.ListWords()[wordCount]) &&
+							WordSearchGrid.Grid[y+count][x-count] == string(WordSearchGrid.ListWords()[wordCount][count]) {
+							word += WordSearchGrid.Grid[y+count][x-count]
+							count++
+						}
+						if word == WordSearchGrid.ListWords()[wordCount] {
+							for i := 0; i < count; i++ {
+								WordSearchGrid.Grid[y+i][x-i] = "\033[" + Light + "" + Color + "m" + WordSearchGrid.Grid[y+i][x-i] + "\033[0m"
 							}
 							WordSearchGrid.DisplayGrid()
 							Save = append(Save, word)
